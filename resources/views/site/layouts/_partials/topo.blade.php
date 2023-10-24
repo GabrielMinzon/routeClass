9aa1fffacadfc4f64d55bb2bcaf9a8fa76e85b7a
GabrielMinzon <112291378+GabrielMinzon@users.noreply.github.com>
2023-10-23 21:23:36 -0300

    Criação das Views, configurando os Controllers e Routes.

---
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -0,0 +1,13 @@
+<div class="topo">
+    <div>
+        <img src="{{ asset('img/logo.php')}}">
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="{{route('site.index')}}"></a>Principal</li>
+            <li><a href="{{route('site.sobrenos')}}"></a>Sobre Nós</li>
+            <li><a href="{{route('site.contato')}}"></a>Contato</li>
+        </ul>
+    </div>
+</div>
